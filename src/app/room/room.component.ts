88d0d126221561fa11e819ce656cdbd796d6d59e
Lakutin Andrey <lakutinit@gmail.com>
2017-04-26 11:26:59 +0300

    Output work perfect now!

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -10,7 +10,6 @@ import { RouterService, BEService } from '../shared';
 export class RoomComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('editor') editor;
   @ViewChild('output') output;
-  text: string;
   userNickname: string;
   userRoom: string;
   saver: any;
@@ -33,18 +32,26 @@ export class RoomComponent implements OnInit, AfterViewInit, OnDestroy {
     });
 
     this.beService.file$.subscribe(file => {
-      this.text = file;
+      this.editor.getEditor().setValue(file, 1);
+    });
+
+    this.beService.output$.subscribe(output => {
+      this.output.getEditor().setValue(output, 1);
+    });
+
+    this.beService.outputError$.subscribe(outputError => {
+      this.output.getEditor().setValue(outputError, -1);
     });
 
     this.saver = setInterval(() => {
-      this.beService.fileSave(this.editor.getEditor().getValue(), this.userRoom);
-      console.log('File saved');
+      this.saveFile();
     }, 5000);
 
     this.beService.getEditorValue(this.userRoom);
   }
 
   ngAfterViewInit() {
+    this.initCustomCommands();
     // Just preventing some console pollution from editor library
     this.editor.getEditor().$blockScrolling = Infinity;
     this.output.getEditor().$blockScrolling = Infinity;
@@ -54,6 +61,22 @@ export class RoomComponent implements OnInit, AfterViewInit, OnDestroy {
     console.log(msg);
   }
 
+  initCustomCommands() {
+    this.editor.getEditor().commands.addCommand({
+      name: 'runAndSave',
+      bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+      exec: () => {
+        this.saveFile();
+        this.runScript();
+      },
+      readOnly: true
+    });
+  }
+
+  runScript() {
+    this.beService.runScript(this.editor.getEditor().getValue(), this.userRoom);
+  }
+
   onEditorChanges() {
     this.beService.updateFile(this.editor.getEditor().getValue(), this.userRoom);
   }
@@ -61,6 +84,11 @@ export class RoomComponent implements OnInit, AfterViewInit, OnDestroy {
   ngOnDestroy() {
     this.beService.leaveRoom();
     clearInterval(this.saver);
+  }
+
+  saveFile() {
+    this.beService.fileSave(this.editor.getEditor().getValue(), this.userRoom);
+    console.log('File saved');
   }
 
 }
